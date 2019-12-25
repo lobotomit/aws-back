aac5b96401289d5682d1cd80c06db3474923007a
Eduardo Reyes <ereyesalvarez@gmail.com>
2019-12-25 01:15:20 +0100

    Add aux method for exist method

--- src/main/java/com/ereyesalvarez/aws/calculator/app/web/api/v1/infrastructure/ItemController.java
+++ src/main/java/com/ereyesalvarez/aws/calculator/app/web/api/v1/infrastructure/ItemController.java
@@ -3,7 +3,7 @@ package com.ereyesalvarez.aws.calculator.app.web.api.v1.infrastructure;
 import com.ereyesalvarez.aws.calculator.app.dto.common.ErrorOut;
 import com.ereyesalvarez.aws.calculator.app.dto.infrastructure.ItemIn;
 import com.ereyesalvarez.aws.calculator.app.dto.infrastructure.ItemOut;
-import com.ereyesalvarez.aws.calculator.app.service.infrastructure.ItemService;
+import com.ereyesalvarez.aws.calculator.app.service.infrastructure.ItemCRUDService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
@@ -18,29 +18,44 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/item")
 public class ItemController {
-    private final ItemService itemService;
+    private final ItemCRUDService itemCRUDService;
 
     @GetMapping("")
     public ResponseEntity<?> getItems() {
-        List<ItemOut> response = itemService.getAll();
+        List<ItemOut> response = itemCRUDService.getAll();
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("{itemId}")
+    public ResponseEntity<?> getOneItem(@PathVariable("itemId") Long itemId){
+        if(!itemCRUDService.itemExist(itemId)){
+            return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ErrorOut(207, "Item not found"));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(itemCRUDService.getOneItem(itemId));
+
     }
     @PostMapping("")
     public ResponseEntity<?> createItem(@RequestBody ItemIn source){
-        ErrorOut errorOut = itemService.validateCreateItem(source);
+        ErrorOut errorOut = itemCRUDService.validateCreateItem(source);
         if(errorOut != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorOut);
         }
-        itemService.createItem(source);
+        itemCRUDService.createItem(source);
         return ResponseEntity.status(HttpStatus.OK).body(new ErrorOut(0,"Ok"));
     }
     @PutMapping("")
     public ResponseEntity<?> updateItem(@RequestBody ItemIn source){
-        ErrorOut errorOut = itemService.validateUpdateItem(source);
+        if(!itemCRUDService.itemExist(source.getId())){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorOut(207, "Item not found"));
+        }
+        ErrorOut errorOut = itemCRUDService.validateUpdateItem(source);
         if(errorOut != null){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorOut);
         }
-        itemService.createItem(source);
+        itemCRUDService.updateItem(source);
         return ResponseEntity.status(HttpStatus.OK).body(new ErrorOut(0,"Ok"));
     }
 
